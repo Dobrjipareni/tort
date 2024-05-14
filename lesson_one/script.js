a0670648ac28d8082cfbd2ba26995a227583879f
unknown <dorozhkinvasya@gmail.com>
2024-05-14 21:24:37 +1000

    qqqwwq

---
+++ lesson_one/script.js
@@ -0,0 +1,70 @@
+// alert("Hello, world"); 
+// // это код
+// function AlertD() {
+//     alert("Hello, world");
+//     /*
+//     dfdfdf
+//     d
+//     fd
+//     f
+//     d
+//     fd
+
+//     */
+// }
+
+// объявление переменой
+// let message;
+
+// message = 2;
+
+// console.log(message)
+
+// let one = 1;
+// let two = 2;
+// let sum = one + two
+
+
+// console.log(one)
+// console.log(two)
+// console.log(sum)
+
+// let bascet = 10;
+
+// let bascet2 = bascet
+
+// const COLOR_RED = "#HEX"
+
+
+//type danjh
+
+// let message = "he'l'l"
+
+// let name = prompt("как тебя зовут")
+// let message = `Привет, ${name}`
+// alert(message)
+// console.log(message)
+
+
+let number1 = 10
+let number2 = 10.30
+
+// string - строка number - число 
+
+
+// bigInt 
+
+let bigInt2 = 12221321323132313231321323132313231323132312n
+
+
+// boolean - правда
+
+// let a = 1
+// let b = false
+
+// let bool1 = 1 > 2
+
+
+// let age = null;
+
+// let age2 = undefined
