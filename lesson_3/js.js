a34358a0a77c9debeed6b0fccfa27e122038a372
unknown <dorozhkinvasya@gmail.com>
2024-05-21 21:10:08 +1000

    fdfdfdfdfd

---
+++ lesson_3/js.js
@@ -0,0 +1,37 @@
+// let a = [1, 2, 3, 4, 5]
+
+// for (let i =0; i != a.length; i++) {
+//     alert(a[i])
+// }
+
+
+// for ('начало'; 'условие'; 'шаг') {
+//     //тело цикла
+// }
+
+
+// let i = 0
+
+// while (true) {
+//     i++
+//     if ( i === 10) {
+//         continue
+//     }
+//     alert(i)
+    
+
+//     if (i === 15) {
+//     alert(i)
+//     }
+// }
+
+// a =[1, 2, 3, 4, 5]
+// b =[9, 8, 7, 6, 5]
+
+
+
+function hellomess() {
+    alert("Всем привет")
+}
+
+hellomess()
