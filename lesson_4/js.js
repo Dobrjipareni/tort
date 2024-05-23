8814ca8826e34ee264296053a684e508f3effc45
unknown <dorozhkinvasya@gmail.com>
2024-05-23 21:10:17 +1000

    sdsdfdfd

---
+++ lesson_4/js.js
@@ -0,0 +1,41 @@
+
+
+// function sayHello() {
+//     alert("Hello")
+// }
+
+// sayHello()
+
+
+// let sayHello = function() {
+//     alert("hi")
+// }
+
+// sayHello()
+
+
+
+// function plus1(number) {
+//     number += 10
+//     alert(number)
+//     if (number < 610) {
+//     plus1(number + 20)
+//     }
+// }
+
+// plus1(0)
+
+
+// let sum2 = (arg1, arg2) => {
+//     alert(arg1 + arg2)
+// }
+
+// sum2(1, 3)
+
+
+let user = {
+    name: "igor",
+    age: 30
+}
+
+alert (user.age)
